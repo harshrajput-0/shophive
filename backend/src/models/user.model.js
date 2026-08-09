0ac8173b84043a4ba44156cac28aae7b2ecb16b6
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-09 13:03:13 +0530

    user model added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        lowercase: true,
+        index: true,
+    }, 
+    password: {
+        type: String, 
+        required: true,
+    },
+    role: {
+        type: String, 
+        enum: ["user", "vendor", "admin"],
+        default: "user",
+    },
+    avatar: {
+        type: String,
+    }, 
+    description: {
+        type: String,
+    }
+})
+
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
