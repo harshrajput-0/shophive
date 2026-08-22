9b48a8a13aea2fd1e7cb0f7ce69ded30593b0779
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-22 09:37:49 +0530

    routes tested with middleware

--- backend/src/middlewares/role.middelware.js
+++ backend/src/middlewares/role.middelware.js
@@ -1,7 +1,7 @@
 // Used after protect middleware
 // Authorize based on user role 
 
-import User from "../models/user.model"
+import User from "../models/user.model.js"
 
 export const authorizeRole = (...roles) => ( req, res, next ) => {
     if (!req.user || !roles.includes(req.user.role)) {
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { protect } from "../middlewares/auth.middleware.js";
+import { authorizeRole } from "../middlewares/role.middelware.js";
 import { registerUser, loginUser, getUsers } from "../controllers/auth.controllers.js";
 
 
@@ -6,6 +8,6 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/users", getUsers);
+router.get("/users", protect, authorizeRole("admin"), getUsers);
 
 export default router;
