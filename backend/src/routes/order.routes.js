aecef742659e894b63be8463c4d7fcc4d8467e5c
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-26 22:32:54 +0530

    order routes updated

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -2,10 +2,15 @@ import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
 import { authorizeRole } from "../middlewares/role.middelware.js";
 
-import { createMockOrder, getMyOrders, getOrders  } from "../controllers/order.controllers";
+import { createMockOrder, getMyOrders, getOrders, getVendorEarning, updateOrderStatus } from "../controllers/order.controllers";
 
 const router = express.Router();
 
 router.post("/mock", protect, createMockOrder);
 router.get("/myorders", protect, getMyOrders);
+router.get("/vendor/earnings", protect, authorizeRole("vendor"), getVendorEarning);
+
 router.get("/", protect, authorizeRole("admin"), getOrders);
+router.get("/:id/status", protect, authorizeRole("admin"), updateOrderStatus);
+
+export default router;
