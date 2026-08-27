3e3459760870ccefbfb64bfee82a3b4f32b1b31a
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-27 07:26:00 +0530

    Updated auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
 import { authorizeRole } from "../middlewares/role.middelware.js";
-import { registerUser, loginUser, getUsers } from "../controllers/auth.controllers.js";
+import { registerUser, loginUser, getUsers, updateUserRole, updateProfile } from "../controllers/auth.controllers.js";
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/profile", protect, updateProfile);
 router.get("/users", protect, authorizeRole("admin"), getUsers);
+router.patch("/users/:id/role", protect, authorizeRole("admin"), updateUserRole);
 
 export default router;
