4507ce971065aec45cf8cd5371a0c8c38b5dcf83
Harsh Rajput <harshrajput0426@gmail.com>
2026-08-27 07:28:47 +0530

    routes added to app

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express"
-import connectDB from "./src/config/db.js"
-import authRoutes from "./src/routes/auth.routes.js"
-import orderRoutes from "./src/routes/order.routes.js"
+import connectDB from "./src/config/db.js";
+import authRoutes from "./src/routes/auth.routes.js";
+import orderRoutes from "./src/routes/order.routes.js";
 import productRoutes from "./src/routes/product.routes.js";
+import paymentRoutes from "./src/routes/payment.routes.js";
+import analyticRoutes from "./src/routes/analytics.routes.js";
 
 const app = express();
 connectDB()
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/analytics", analyticRoutes);
 
 
 export default app;
